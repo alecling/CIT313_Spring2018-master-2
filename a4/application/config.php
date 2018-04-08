20feb99cfdc3bf194f2fc56345f6896ce80c8cc9
alecling <alecling@umail.iu.edu>
2018-04-08 16:54:01 -0400

    d
    d

---
+++ a4/application/config.php
@@ -0,0 +1,9 @@
+<?php
+define('DEFAULT_VIEW', 'home');//set this to any page to be the default home page
+define('BASE_URL', 'http://corsair.cs.iupui.edu:21441/CIT313/a4/');
+
+//database info
+define('DB_HOST', 'localhost');
+define('DB_USER', 'alecling');
+define('DB_PASS', 'alecling');
+define('DB_NAME', 'alecling_db');
