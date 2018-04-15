b72cc80836a475f7432d4abf1c7061eabe55a093
alecling <alecling@umail.iu.edu>
2018-04-15 16:13:16 -0400

    d

--- a1/Debug-Exercise-master/webconfig.php
+++ a1/Debug-Exercise-master/webconfig.php
@@ -1,4 +1,4 @@
 <?php
 
-define('ABSOLUTE_PATH', '/home/joldela/htdocs/CIT313/Fall2017/a1');
+define('ABSOLUTE_PATH', '/home/alecling/htdocs/CIT313/a1');
 define('URL_ROOT', 'http://corsair.cs.iupui.edu:21441/CIT313/a1/');
